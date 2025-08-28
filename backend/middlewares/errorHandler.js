8e3c90c0291844e1e55e7b564ec8cb57aef79377
adityayudantira <adit818@gmail.com>
2025-08-28 16:48:22 +0700

    Update errorHandler

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,13 +1,11 @@
-// Middleware handle error untuk duplicate key MongoDB
-function handleMongoDuplicateKeyError(err, req, res, next) {
+// middlewares/errorHandler.js
+
+export function handleMongoDuplicateKeyError(err, req, res, next) {
   if (err.code && err.code === 11000) {
-    // Ambil field yang duplikat
     const field = Object.keys(err.keyPattern)[0];
     return res.status(409).json({
       message: `❌ ${field} sudah terdaftar`
     });
   }
-  next(err); // teruskan error lain
+  next(err);
 }
-
-module.exports = { handleMongoDuplicateKeyError };
